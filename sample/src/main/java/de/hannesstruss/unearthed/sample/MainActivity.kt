08e2dd41ed218107f2947ece75b9a203567d5ecb
Hannes Struß <x@hannesstruss.de>
2021-04-10 13:56:34 +0200

    Switch kotlin-android-extensions for new Parcelize plugin

--- sample/src/main/java/de/hannesstruss/unearthed/sample/MainActivity.kt
+++ sample/src/main/java/de/hannesstruss/unearthed/sample/MainActivity.kt
@@ -1,20 +1,23 @@
 package de.hannesstruss.unearthed.sample
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import de.hannesstruss.unearthed.Unearthed
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+  @SuppressLint("SetTextI18n")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
 
-        txt_content.text = "Fresh"
+    val txtContent = findViewById<TextView>(R.id.txt_content)
+    txtContent.text = "Fresh"
 
-        Unearthed.onProcessRestored { graveyard ->
-            txt_content.text = "Process restored ${graveyard.gravestones.size} times."
-        }
+    Unearthed.onProcessRestored { graveyard ->
+      txtContent.text = "Process restored ${graveyard.gravestones.size} times."
     }
+  }
 }
